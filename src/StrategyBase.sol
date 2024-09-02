36fbb115ccd4d9fa383dcabf9ba7b5436c666bce
Hovooo <avakiandev@gmail.com>
2024-09-02 13:19:56 +0400

    Improved comments for StrategyBase Contract

--- src/StrategyBase.sol
+++ src/StrategyBase.sol
@@ -11,32 +11,55 @@ import { IReceiptToken } from "@jigsaw/src/interfaces/core/IReceiptToken.sol";
 import { IStrategy } from "@jigsaw/src/interfaces/core/IStrategy.sol";
 import { IStrategyManager } from "@jigsaw/src/interfaces/core/IStrategyManager.sol";
 
-/// @title StrategyBase contract used for any Aave strategy
-/// @author Cosmin Grigore (@gcosmintech)
+/**
+ * @title StrategyBase contract used for any Aave strategy.
+ * @author Cosmin Grigore (@gcosmintech)
+ */
 abstract contract StrategyBase is Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
-    /// @notice emitted when a new underlying is added to the whitelist
+    /**
+     * @notice Emitted when a new underlying is added to the whitelist.
+     */
     event UnderlyingAdded(address indexed newAddress);
-    /// @notice emitted when a new underlying is removed from the whitelist
+
+    /**
+     * @notice Emitted when a new underlying is removed from the whitelist.
+     */
     event UnderlyingRemoved(address indexed old);
-    /// @notice emitted when the address is updated
+
+    /**
+     * @notice Emitted when the address is updated.
+     */
     event StrategyManagerUpdated(address indexed old, address indexed newAddress);
-    /// @notice emitted when funds are saved in case of an emergency
+
+    /**
+     * @notice Emitted when funds are saved in case of an emergency.
+     */
     event SavedFunds(address indexed token, uint256 amount);
-    /// @notice emitted when receipt tokens are minted
+
+    /**
+     * @notice Emitted when receipt tokens are minted.
+     */
     event ReceiptTokensMinted(address indexed receipt, uint256 amount);
-    /// @notice emitted when receipt tokens are burned
+
+    /**
+     * @notice Emitted when receipt tokens are burned.
+     */
     event ReceiptTokensBurned(address indexed receipt, uint256 amount);
 
-    /// @notice contract that contains the address of the manager contract
+    /**
+     * @notice Contract that contains the address of the manager contract.
+     */
     IManagerContainer public managerContainer;
 
     constructor(address _owner) Ownable(_owner) { }
 
-    /// @notice save funds
-    /// @param _token token address
-    /// @param _amount token amount
+    /**
+     * @notice Save funds.
+     *  @param _token Token address.
+     *  @param _amount Token amount.
+     */
     function emergencySave(
         address _token,
         uint256 _amount
@@ -47,15 +70,29 @@ abstract contract StrategyBase is Ownable, ReentrancyGuard {
         emit SavedFunds(_token, _amount);
     }
 
+    /**
+     * @notice Retrieves the Manager Contract instance from the Manager Container.
+     * @return IManager The Manager Contract instance.
+     */
     function _getManager() internal view returns (IManager) {
         return IManager(managerContainer.manager());
     }
 
+    /**
+     * @notice Retrieves the Strategy Manager Contract instance from the Manager Contract.
+     * @return IStrategyManager The Strategy Manager contract instance.
+     */
     function _getStrategyManager() internal view returns (IStrategyManager) {
         return IStrategyManager(_getManager().strategyManager());
     }
 
-    /// @notice mints an amount of receipt tokens
+    /**
+     * @notice Mints an amount of receipt tokens.
+     *  @param _receiptToken The receipt token contract.
+     *  @param _recipient The recipient of the minted tokens.
+     *  @param _amount The amount of tokens to mint.
+     *  @param _tokenDecimals The decimals of the token.
+     */
     function _mint(IReceiptToken _receiptToken, address _recipient, uint256 _amount, uint256 _tokenDecimals) internal {
         uint256 realAmount = _amount;
         if (_tokenDecimals > 18) {
@@ -67,7 +104,14 @@ abstract contract StrategyBase is Ownable, ReentrancyGuard {
         emit ReceiptTokensMinted(_recipient, realAmount);
     }
 
-    /// @notice burns an amount of receipt tokens
+    /**
+     * @notice Burns an amount of receipt tokens.
+     *  @param _receiptToken The receipt token contract.
+     *  @param _recipient The recipient whose tokens will be burned.
+     *  @param _shares The amount of shares to burn.
+     *  @param _totalShares The total shares in the system.
+     *  @param _tokenDecimals The decimals of the token.
+     */
     function _burn(
         IReceiptToken _receiptToken,
         address _recipient,
@@ -88,21 +132,37 @@ abstract contract StrategyBase is Ownable, ReentrancyGuard {
         emit ReceiptTokensBurned(_recipient, realAmount);
     }
 
-    // @dev renounce ownership override to avoid losing contract's ownership
+    /**
+     * @dev Renounce ownership override to avoid losing contract's ownership.
+     */
     function renounceOwnership() public pure virtual override {
         revert("1000");
     }
 
+    /**
+     * @notice Ensures that the caller is the strategy manager.
+     * @dev Reverts with "1000" if the caller is not the strategy manager.
+     */
     modifier onlyStrategyManager() {
         require(msg.sender == address(_getStrategyManager()), "1000");
         _;
     }
 
+    /**
+     * @notice Ensures that the provided amount is valid (greater than 0).
+     * @dev Reverts with "2001" if the amount is 0 or less.
+     * @param _amount The amount to validate.
+     */
     modifier onlyValidAmount(uint256 _amount) {
         require(_amount > 0, "2001");
         _;
     }
 
+    /**
+     * @notice Ensures that the provided address is valid (not the zero address).
+     * @dev Reverts with "3000" if the address is the zero address.
+     * @param _addr The address to validate.
+     */
     modifier onlyValidAddress(address _addr) {
         require(_addr != address(0), "3000");
         _;
