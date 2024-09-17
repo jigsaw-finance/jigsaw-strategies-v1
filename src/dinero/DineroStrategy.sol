0e84594ed93d50ecadc402474934157ecb8f57e4
Tigran <traqel@gmail.com>
2024-09-17 15:13:01 +0400

    Add Dinero stuff

---
+++ src/dinero/DineroStrategy.sol
@@ -0,0 +1,373 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {IERC20, IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import {IHolding} from "@jigsaw/src/interfaces/core/IHolding.sol";
+import {IManagerContainer} from "@jigsaw/src/interfaces/core/IManagerContainer.sol";
+import {IReceiptToken} from "@jigsaw/src/interfaces/core/IReceiptToken.sol";
+import {IStrategy} from "@jigsaw/src/interfaces/core/IStrategy.sol";
+
+import {OperationsLib} from "../libraries/OperationsLib.sol";
+import {StrategyConfigLib} from "../libraries/StrategyConfigLib.sol";
+
+import {IStakerLight} from "../staker/interfaces/IStakerLight.sol";
+import {IStakerLightFactory} from "../staker/interfaces/IStakerLightFactory.sol";
+
+import {StrategyBaseUpgradeable} from "../StrategyBaseUpgradeable.sol";
+
+/**
+ * @title DineroStrategy
+ * @dev Strategy used for PirexEth.
+ * @author Hovooo (@hovooo)
+ */
+contract DineroStrategy is IStrategy, StrategyBaseUpgradeable {
+    using SafeERC20 for IERC20;
+
+    // -- Custom types --
+
+    /**
+     * @notice Struct for the initializer params.
+     */
+    struct InitializerParams {
+        address owner; // The address of the initial owner of the Strategy contract
+        address managerContainer; // The address of the contract that contains the manager contract
+        address stakerFactory; // The address of the StakerLightFactory contract
+        address pirexEth; // The address of the PirexEth
+        address jigsawRewardToken; // The address of the Jigsaw reward token associated with the strategy
+        uint256 jigsawRewardDuration; // The address of the initial Jigsaw reward distribution duration for the strategy
+        address tokenIn; // The address of the LP token
+        address tokenOut; // The address of the PirexEth receipt token (pxEth)
+    }
+
+    /**
+     * @notice Struct containing parameters for a withdrawal operation.
+     */
+    struct WithdrawParams {
+        uint256 shareRatio; // The share ratio representing the proportion of the total investment owned by the user.
+        uint256 investment; // The amount of funds invested by the user.
+        uint256 balanceBefore; // The user's balance in the system before the withdrawal transaction.
+        uint256 balanceAfter; // The user's balance in the system after the withdrawal transaction is completed.
+    }
+    // -- Errors --
+
+    error OperationNotSupported();
+
+    // -- Events --
+
+    /**
+     * @notice Emitted when a performance fee is taken.
+     * @param token The token from which the fee is taken.
+     * @param feeAddress The address that receives the fee.
+     * @param amount The amount of the fee.
+     */
+    event FeeTaken(address indexed token, address indexed feeAddress, uint256 amount);
+
+    /**
+     * @notice Emitted when the PirexEth address is updated.
+     * @param _old The old PirexEth address.
+     * @param _new The new PirexEth address.
+     */
+    event pirexEthUpdated(address indexed _old, address indexed _new);
+
+    // -- State variables --
+
+    /**
+     * @notice The LP token address.
+     */
+    address public override tokenIn;
+
+    /**
+     * @notice The PirexEth receipt token address.
+     */
+    address public override tokenOut;
+
+    /**
+     * @notice The reward token offered to users.
+     */
+    address public override rewardToken;
+
+    /**
+     * @notice The receipt token associated with this strategy.
+     */
+    IReceiptToken public override receiptToken;
+
+    /**
+     * @notice The PirexEth contract.
+     */
+    IPirexEth public pirexEth;
+
+    /**
+     * @notice The Jigsaw Rewards Controller contract.
+     */
+    IStakerLight public jigsawStaker;
+
+    /**
+     * @notice The number of decimals of the strategy's shares.
+     */
+    uint256 public override sharesDecimals;
+
+    /**
+     * @notice The total investments in the strategy.
+     */
+    uint256 public totalInvestments;
+
+    /**
+     * @notice A mapping that stores participant details by address.
+     */
+    mapping(address => IStrategy.RecipientInfo) public override recipients;
+
+    // -- Constructor --
+
+    constructor() {
+        _disableInitializers();
+    }
+
+    // -- Initialization --
+
+    /**
+     * @notice Initializer for the Dinero Strategy.
+     */
+    function initialize(InitializerParams memory _params) public initializer {
+        require(_params.managerContainer != address(0), "3065");
+        require(_params.pirexEth != address(0), "3036");
+        require(_params.tokenIn != address(0), "3000");
+        require(_params.tokenOut != address(0), "3000");
+
+        __StrategyBase_init({_initialOwner: _params.owner});
+
+        managerContainer = IManagerContainer(_params.managerContainer);
+        pirexEth = IPirexEth(_params.pirexEth);
+        tokenIn = _params.tokenIn;
+        tokenOut = _params.tokenOut;
+        sharesDecimals = IERC20Metadata(_params.tokenOut).decimals();
+
+        receiptToken = IReceiptToken(
+            StrategyConfigLib.configStrategy({
+                _receiptTokenFactory: _getManager().receiptTokenFactory(),
+                _receiptTokenName: "PirexEth Strategy Receipt Token",
+                _receiptTokenSymbol: "pxEth"
+            })
+        );
+
+        jigsawStaker = IStakerLight(
+            IStakerLightFactory(_params.stakerFactory).createStakerLight({
+                _initialOwner: _params.owner,
+                _holdingManager: _getManager().holdingManager(),
+                _tokenIn: address(receiptToken),
+                _rewardToken: _params.jigsawRewardToken,
+                _strategy: address(this),
+                _rewardsDuration: _params.jigsawRewardDuration
+            })
+        );
+    }
+
+    // -- User-specific Methods --
+
+    /**
+     * @notice Deposits funds into the strategy.
+     *
+     * @dev Some strategies won't return any receipt tokens; in this case, 'tokenOutAmount' will be 0.
+     * @dev 'tokenInAmount' will be equal to '_amount' if '_asset' is the same as the strategy's 'tokenIn()'.
+     *
+     * @param _asset The token to be invested.
+     * @param _amount The amount of the token to be invested.
+     * @param _recipient The address on behalf of which the funds are deposited.
+     * @param _data Extra data, e.g., a referral code.
+     *
+     * @return The amount of receipt tokens obtained.
+     * @return The amount of the 'tokenIn()' token.
+     */
+    function deposit(
+        address _asset,
+        uint256 _amount,
+        address _recipient,
+        bytes calldata _data
+    ) external override onlyValidAmount(_amount) onlyStrategyManager nonReentrant returns (uint256, uint256) {
+        require(_asset == tokenIn, "3001");
+
+        uint16 refCode = 0;
+        if (_data.length > 0) {
+            refCode = abi.decode(_data, (uint16));
+        }
+
+        IHolding(_recipient).transfer({_token: _asset, _to: address(this), _amount: _amount});
+
+        uint256 balanceBefore = IERC20(tokenOut).balanceOf(_recipient);
+        OperationsLib.safeApprove({token: _asset, to: address(pirexEth), value: _amount});
+        pirexEth.deposit({receiver: _recipient, shouldCompound: false});
+        uint256 balanceAfter = IERC20(tokenOut).balanceOf(_recipient);
+
+        recipients[_recipient].investedAmount += balanceAfter - balanceBefore;
+        recipients[_recipient].totalShares += balanceAfter - balanceBefore;
+        totalInvestments += balanceAfter - balanceBefore;
+
+        _mint({
+            _receiptToken: receiptToken,
+            _recipient: _recipient,
+            _amount: balanceAfter - balanceBefore,
+            _tokenDecimals: IERC20Metadata(tokenOut).decimals()
+        });
+
+        jigsawStaker.deposit({_user: _recipient, _amount: recipients[_recipient].investedAmount});
+
+        emit Deposit({
+            asset: _asset,
+            tokenIn: tokenIn,
+            assetAmount: _amount,
+            tokenInAmount: _amount,
+            shares: balanceAfter - balanceBefore,
+            recipient: _recipient
+        });
+
+        return (balanceAfter - balanceBefore, _amount);
+    }
+
+    /**
+     * @notice Withdraws deposited funds from the strategy.
+     *
+     * @dev Some strategies will only allow the tokenIn to be withdrawn.
+     * @dev 'assetAmount' will be equal to 'tokenInAmount' if '_asset' is the same as the strategy's 'tokenIn()'.
+     *
+     * @param _shares The amount of shares to withdraw.
+     * @param _recipient The address on behalf of which the funds are withdrawn.
+     * @param _asset The token to be withdrawn.
+     *
+     * @return The amount of the asset obtained from the operation.
+     * @return The amount of the 'tokenIn()' token.
+     */
+    function withdraw(
+        uint256 _shares,
+        address _recipient,
+        address _asset,
+        bytes calldata
+    ) external override onlyStrategyManager nonReentrant returns (uint256, uint256) {
+        require(_asset == tokenIn, "3001");
+        require(_shares <= IERC20(tokenOut).balanceOf(_recipient), "2002");
+
+        WithdrawParams memory params =
+                        WithdrawParams({shareRatio: 0, investment: 0, balanceBefore: 0, balanceAfter: 0});
+
+        params.shareRatio = OperationsLib.getRatio({
+            numerator: _shares,
+            denominator: recipients[_recipient].totalShares,
+            precision: IERC20Metadata(tokenOut).decimals()
+        });
+
+        _burn({
+            _receiptToken: receiptToken,
+            _recipient: _recipient,
+            _shares: _shares,
+            _totalShares: recipients[_recipient].totalShares,
+            _tokenDecimals: IERC20Metadata(tokenOut).decimals()
+        });
+
+        params.investment =
+            (recipients[_recipient].investedAmount * params.shareRatio) / (10 ** IERC20Metadata(tokenOut).decimals());
+
+        params.balanceBefore = IERC20(tokenIn).balanceOf(_recipient);
+        (bool success, bytes memory returnData) = IHolding(_recipient).genericCall({
+            _contract: address(pirexEth),
+            _call: abi.encodeWithSignature(
+                "withdraw(address,uint256)",
+                _recipient, // receiverOfUnderlying
+                _shares // amount of underlying to redeem
+            )
+        });
+        // Assert the call succeeded.
+        require(success, OperationsLib.getRevertMsg(returnData));
+        params.balanceAfter = IERC20(tokenIn).balanceOf(_recipient);
+
+        _extractTokenInRewards({
+            _ratio: params.shareRatio,
+            _result: params.balanceAfter - params.balanceBefore,
+            _recipient: _recipient,
+            _decimals: IERC20Metadata(tokenOut).decimals()
+        });
+
+        jigsawStaker.withdraw({_user: _recipient, _amount: recipients[_recipient].investedAmount});
+
+        recipients[_recipient].totalShares =
+            _shares > recipients[_recipient].totalShares ? 0 : recipients[_recipient].totalShares - _shares;
+        recipients[_recipient].investedAmount = params.investment > recipients[_recipient].investedAmount
+            ? 0
+            : recipients[_recipient].investedAmount - params.investment;
+        totalInvestments =
+            params.balanceAfter - params.balanceBefore > totalInvestments ? 0 : totalInvestments - params.investment;
+
+        emit Withdraw({
+            asset: _asset,
+            recipient: _recipient,
+            shares: _shares,
+            amount: params.balanceAfter - params.balanceBefore
+        });
+        return (params.balanceAfter - params.balanceBefore, params.investment);
+    }
+
+    /**
+     * @notice Claims rewards from the PirexEth.
+     * @return The amounts of rewards claimed.
+     * @return The addresses of the reward tokens.
+     */
+    function claimRewards(
+        address,
+        bytes calldata
+    ) external pure override returns (uint256[] memory, address[] memory) {
+        revert OperationNotSupported();
+    }
+
+    // -- Getters --
+
+    /**
+     * @notice Returns the address of the receipt token.
+     */
+    function getReceiptTokenAddress() external view override returns (address) {
+        return address(receiptToken);
+    }
+
+    // -- Utilities --
+
+    /**
+     * @notice Sends tokenIn rewards to the fee address.
+     *
+     * @param _ratio The ratio of the shares to total shares.
+     * @param _result The _result of the balance change.
+     * @param _recipient The address of the recipient.
+     * @param _decimals The number of decimals of the token.
+     */
+    function _extractTokenInRewards(uint256 _ratio, uint256 _result, address _recipient, uint256 _decimals) internal {
+        if (_ratio > (10 ** _decimals) && _result < recipients[_recipient].investedAmount) return;
+        uint256 rewardAmount = 0;
+        if (_ratio >= (10 ** _decimals)) rewardAmount = _result - recipients[_recipient].investedAmount;
+        if (rewardAmount == 0) return;
+
+        (uint256 performanceFee,,) = _getStrategyManager().strategyInfo(address(this));
+        uint256 fee = OperationsLib.getFeeAbsolute(rewardAmount, performanceFee);
+
+        if (fee > 0) {
+            address feeAddr = _getManager().feeAddress();
+            emit FeeTaken(tokenIn, feeAddr, fee);
+            IHolding(_recipient).transfer(tokenIn, feeAddr, fee);
+        }
+    }
+}
+
+interface IPirexEth {
+    /**
+    * @notice Handle pxETH minting in return for ETH deposits
+    * @dev    This function handles the minting of pxETH in return for ETH deposits.
+    * @param  receiver        address  Receiver of the minted pxETH or apxEth
+    * @param  shouldCompound  bool     Whether to also compound into the vault
+    * @return postFeeAmount   uint256  pxETH minted for the receiver
+    * @return feeAmount       uint256  pxETH distributed as fees
+    */
+    function deposit(address receiver, bool shouldCompound) external payable
+    returns (uint256 postFeeAmount, uint256 feeAmount);
+
+    /**
+     * @dev Current token balance
+     * @param user to get balance of
+     */
+    function balanceOf(address user) external view returns (uint256);
+}
---
+++ test/dinero/DineroStrategy_fork.t.sol
@@ -0,0 +1,123 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+import "../fixtures/BasicContractsFixture.t.sol";
+
+import { ERC20Mock } from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import { IERC20, IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+
+import { IPirexEth } from "../../src/dinero/DineroStrategy.sol";
+import { DineroStrategy } from "../../src/dinero/DineroStrategy.sol";
+import { StakerLight } from "../../src/staker/StakerLight.sol";
+import { StakerLightFactory } from "../../src/staker/StakerLightFactory.sol";
+
+IPirexEth constant PIREX_ETH = IPirexEth(0xD664b74274DfEB538d9baC494F3a4760828B02b0);
+
+contract DineroStrategyTest is Test, BasicContractsFixture {
+    // TODO: Mainnet PirexEth
+    address internal tokenIn = 0xD664b74274DfEB538d9baC494F3a4760828B02b0;
+    // TODO: Dinero iweETH-wstETH token is the same as the pool due to Vault architecture
+    address internal tokenOut = address(PIREX_ETH);
+
+    DineroStrategy internal strategy;
+
+    function setUp() public {
+        init();
+
+        address jRewards = address(new ERC20Mock());
+        address stakerFactory = address(new StakerLightFactory({ _initialOwner: OWNER }));
+        address strategyImplementation = address(new DineroStrategy());
+
+        DineroStrategy.InitializerParams memory initParams = DineroStrategy.InitializerParams({
+            owner: OWNER,
+            managerContainer: address(managerContainer),
+            stakerFactory: address(stakerFactory),
+            pirexEth: address(PIREX_ETH),
+            jigsawRewardToken: jRewards,
+            jigsawRewardDuration: 60 days,
+            tokenIn: tokenIn,
+            tokenOut: tokenOut
+        });
+
+        bytes memory data = abi.encodeCall(DineroStrategy.initialize, initParams);
+        address proxy = address(new ERC1967Proxy(strategyImplementation, data));
+        strategy = DineroStrategy(proxy);
+
+        // Add tested strategy to the StrategyManager for integration testing purposes
+        vm.startPrank((OWNER));
+        manager.whitelistToken(tokenIn);
+        strategyManager.addStrategy(address(strategy));
+
+        SharesRegistry tokenInSharesRegistry = new SharesRegistry(
+            OWNER, address(managerContainer), address(tokenIn), address(usdcOracle), bytes(""), 50_000
+        );
+        stablesManager.registerOrUpdateShareRegistry(address(tokenInSharesRegistry), address(tokenIn), true);
+        registries[address(tokenIn)] = address(tokenInSharesRegistry);
+        vm.stopPrank();
+    }
+
+//    // Tests if deposit works correctly when authorized
+//    function test_dinero_deposit_when_authorized(address user, uint256 _amount) public notOwnerNotZero(user) {
+//        uint256 amount = bound(_amount, 1e18, 10e18);
+//
+//        address userHolding = initiateUser(user, tokenIn, amount);
+//        uint256 balanceBefore = IERC20(tokenOut).balanceOf(userHolding);
+//
+//        // Invest into the tested strategy vie strategyManager
+//        vm.prank(user, user);
+//        (uint256 receiptTokens, uint256 tokenInAmount) = strategyManager.invest(tokenIn, address(strategy), amount, "");
+//
+//        uint256 balanceAfter = IERC20(tokenOut).balanceOf(userHolding);
+//        uint256 expectedShares = balanceAfter - balanceBefore;
+//        (uint256 investedAmount, uint256 totalShares) = strategy.recipients(userHolding);
+//
+//        assertApproxEqRel(balanceAfter, balanceBefore + amount, 0.01e18, "Wrong balance in ION after stake");
+//        assertEq(receiptTokens, expectedShares, "Incorrect receipt tokens returned");
+//        assertEq(tokenInAmount, amount, "Incorrect tokenInAmount returned");
+//        assertEq(investedAmount, expectedShares, "Recipient invested amount mismatch");
+//        assertEq(totalShares, expectedShares, "Recipient total shares mismatch");
+//        assertEq(strategy.totalInvestments(), expectedShares, "Total investments mismatch");
+//    }
+//
+//    // Tests if withdraw works correctly when authorized
+//    function test_ion_withdraw_when_authorized(address user, uint256 _amount) public notOwnerNotZero(user) {
+//        uint256 amount = bound(_amount, 1e18, 10e18);
+//
+//        // Mock values and setup necessary approvals and balances for the test
+//        address userHolding = initiateUser(user, tokenIn, amount);
+//
+//        // Invest into the tested strategy vie strategyManager
+//        vm.prank(user, user);
+//        strategyManager.invest(tokenIn, address(strategy), amount, "");
+//
+//        (, uint256 totalShares) = strategy.recipients(userHolding);
+//
+//        // Mock the recipient’s shares balance
+//        uint256 balanceBefore = IERC20(tokenIn).balanceOf(userHolding);
+//
+//        vm.prank(user, user);
+//        (uint256 assetAmount, uint256 tokenInAmount) = strategyManager.claimInvestment({
+//            _holding: userHolding,
+//            _strategy: address(strategy),
+//            _shares: totalShares,
+//            _asset: tokenIn,
+//            _data: ""
+//        });
+//
+//        uint256 balanceAfter = IERC20(tokenIn).balanceOf(userHolding);
+//
+//        uint256 expectedWithdrawal = balanceBefore > balanceAfter ? 0 : balanceAfter - balanceBefore;
+//
+//        (, uint256 totalSharesAfter) = strategy.recipients(userHolding);
+//
+//        // Assert statements with reasons
+//        assertEq(assetAmount, expectedWithdrawal, "Incorrect asset amount returned");
+//        assertApproxEqAbs(tokenInAmount, expectedWithdrawal, 1, "Incorrect tokenInAmount returned");
+//        assertEq(totalSharesAfter, 0, "Recipient total shares mismatch after withdrawal");
+//        assertEq(strategy.totalInvestments(), 0, "Total investments mismatch after withdrawal");
+//    }
+}
