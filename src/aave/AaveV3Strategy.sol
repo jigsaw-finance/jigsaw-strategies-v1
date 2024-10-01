6803406f4a947d3a9e7bab7ce2e3bf45efe9006b
Hovooo <avakiandev@gmail.com>
2024-10-01 13:39:46 +0400

    Fixed typo in AaveV3Strategy

--- src/aave/AaveV3Strategy.sol
+++ src/aave/AaveV3Strategy.sol
@@ -253,7 +253,7 @@ contract AaveV3Strategy is IStrategy, StrategyBaseUpgradeable {
 
         IHolding(_recipient).transfer({ _token: tokenOut, _to: address(this), _amount: _shares });
 
-        uint256 balanceBefore = IERC20¸(tokenIn).balanceOf(_recipient);
+        uint256 balanceBefore = IERC20(tokenIn).balanceOf(_recipient);
         lendingPool.withdraw({ asset: _asset, amount: _shares, to: _recipient });
         uint256 balanceAfter = IERC20(tokenIn).balanceOf(_recipient);
 
