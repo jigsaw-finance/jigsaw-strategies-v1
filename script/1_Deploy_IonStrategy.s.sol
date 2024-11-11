e29c1074d7da2a2ce9af59c571e7acdd7113ebbe
Hovooo <avakiandev@gmail.com>
2024-11-11 12:53:20 +0530

    Added IonStrategy deployment script [WIP]

---
+++ script/1_Deploy_IonStrategy.s.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.25;
+
+import { Script, console2 as console } from "forge-std/Script.sol";
+
+import { IonStrategy } from "../src/ion/IonStrategy.sol";
+
+contract Deploy_IonStrategy is Script {
+    function run() external { }
+}
