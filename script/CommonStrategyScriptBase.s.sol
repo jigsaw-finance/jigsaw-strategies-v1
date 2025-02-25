6daab01e1abae965c3c0f29feefe5fa3b7df5abc
Hovooo <avakiandev@gmail.com>
2025-02-25 11:25:01 +0530

    Updated CommonStrategyScriptBase

--- script/CommonStrategyScriptBase.s.sol
+++ script/CommonStrategyScriptBase.s.sol
@@ -48,6 +48,8 @@ contract CommonStrategyScriptBase is Script {
         address tokenOut; // The address of the PirexEth receipt token (pxEth)
     }
 
+    uint256 constant DEFAULT_REWARDS_DURATION = 75 days;
+
     bytes32 constant AAVE_STRATEGY = keccak256("AaveV3Strategy");
     bytes32 constant ION_STRATEGY = keccak256("IonStrategy");
     bytes32 constant PENDLE_STRATEGY = keccak256("PendleStrategy");
@@ -220,7 +222,7 @@ contract CommonStrategyScriptBase is Script {
         aaveStrategyParams.push(
             AaveStrategyParams({
                 rewardToken: address(0),
-                jigsawRewardDuration: 365 days,
+                jigsawRewardDuration: DEFAULT_REWARDS_DURATION,
                 tokenIn: 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, //wETH
                 tokenOut: 0x4d5F47FA6A74757f35C14fD3a6Ef8E3C9BC514E8
             })
@@ -229,7 +231,7 @@ contract CommonStrategyScriptBase is Script {
         aaveStrategyParams.push(
             AaveStrategyParams({
                 rewardToken: address(0),
-                jigsawRewardDuration: 365 days,
+                jigsawRewardDuration: DEFAULT_REWARDS_DURATION,
                 tokenIn: 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0, //wstETH
                 tokenOut: 0x0B925eD163218f6662a35e0f0371Ac234f9E9371
             })
@@ -238,7 +240,7 @@ contract CommonStrategyScriptBase is Script {
         aaveStrategyParams.push(
             AaveStrategyParams({
                 rewardToken: address(0),
-                jigsawRewardDuration: 365 days,
+                jigsawRewardDuration: DEFAULT_REWARDS_DURATION,
                 tokenIn: 0xCd5fE23C85820F7B72D0926FC9b05b43E359b7ee, //weETH
                 tokenOut: 0xBdfa7b7893081B35Fb54027489e2Bc7A38275129
             })
@@ -247,7 +249,7 @@ contract CommonStrategyScriptBase is Script {
         aaveStrategyParams.push(
             AaveStrategyParams({
                 rewardToken: address(0),
-                jigsawRewardDuration: 365 days,
+                jigsawRewardDuration: DEFAULT_REWARDS_DURATION,
                 tokenIn: 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599, //wBTC
                 tokenOut: 0x5Ee5bf7ae06D1Be5997A1A72006FE6C607eC6DE8
             })
@@ -256,7 +258,7 @@ contract CommonStrategyScriptBase is Script {
         aaveStrategyParams.push(
             AaveStrategyParams({
                 rewardToken: address(0),
-                jigsawRewardDuration: 365 days,
+                jigsawRewardDuration: DEFAULT_REWARDS_DURATION,
                 tokenIn: 0xdAC17F958D2ee523a2206206994597C13D831ec7, //USDT
                 tokenOut: 0x23878914EFE38d27C4D67Ab83ed1b93A74D4086a
             })
@@ -265,7 +267,7 @@ contract CommonStrategyScriptBase is Script {
         aaveStrategyParams.push(
             AaveStrategyParams({
                 rewardToken: address(0),
-                jigsawRewardDuration: 365 days,
+                jigsawRewardDuration: DEFAULT_REWARDS_DURATION,
                 tokenIn: 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48, //USDC
                 tokenOut: 0x98C23E9d8f34FEFb1B7BD6a91B7FF122F4e16F5c
             })
@@ -274,9 +276,18 @@ contract CommonStrategyScriptBase is Script {
         aaveStrategyParams.push(
             AaveStrategyParams({
                 rewardToken: address(0),
-                jigsawRewardDuration: 365 days,
+                jigsawRewardDuration: DEFAULT_REWARDS_DURATION,
                 tokenIn: 0x6B175474E89094C44Da98b954EedeAC495271d0F, //DAI
                 tokenOut: 0x018008bfb33d285247A21d44E50697654f754e63
+            })
+        );
+
+        aaveStrategyParams.push(
+            AaveStrategyParams({
+                rewardToken: address(0),
+                jigsawRewardDuration: DEFAULT_REWARDS_DURATION,
+                tokenIn: 0x9D39A5DE30e57443BfF2A8307A4256c8797A3497, //sUSDe
+                tokenOut: 0x4579a27aF00A62C0EB156349f31B345c08386419
             })
         );
     }
@@ -286,7 +297,7 @@ contract CommonStrategyScriptBase is Script {
         ionStrategyParams.push(
             IonStrategyParams({
                 ionPool: 0x0000000000eaEbd95dAfcA37A39fd09745739b78, //wstETH/weETH Ion Pool
-                jigsawRewardDuration: 365 days,
+                jigsawRewardDuration: DEFAULT_REWARDS_DURATION,
                 tokenIn: 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0
             })
         );
@@ -294,7 +305,7 @@ contract CommonStrategyScriptBase is Script {
         ionStrategyParams.push(
             IonStrategyParams({
                 ionPool: 0x00000000007C8105548f9d0eE081987378a6bE93, //wstETH/rswETH Ion Pool
-                jigsawRewardDuration: 365 days,
+                jigsawRewardDuration: DEFAULT_REWARDS_DURATION,
                 tokenIn: 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0
             })
         );
@@ -302,7 +313,7 @@ contract CommonStrategyScriptBase is Script {
         ionStrategyParams.push(
             IonStrategyParams({
                 ionPool: 0x0000000000E33e35EE6052fae87bfcFac61b1da9, //wstETH/rsETH Ion Pool
-                jigsawRewardDuration: 365 days,
+                jigsawRewardDuration: DEFAULT_REWARDS_DURATION,
                 tokenIn: 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0
             })
         );
@@ -310,7 +321,7 @@ contract CommonStrategyScriptBase is Script {
         ionStrategyParams.push(
             IonStrategyParams({
                 ionPool: 0x00000000008a3A77bd91bC738Ed2Efaa262c3763, //wETH/ezETH Ion Pool
-                jigsawRewardDuration: 365 days,
+                jigsawRewardDuration: DEFAULT_REWARDS_DURATION,
                 tokenIn: 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
             })
         );
@@ -322,7 +333,7 @@ contract CommonStrategyScriptBase is Script {
             ReservoirStablecoinStrategyParams({
                 creditEnforcer: 0x04716DB62C085D9e08050fcF6F7D775A03d07720,
                 pegStabilityModule: 0x4809010926aec940b550D34a46A52739f996D75D,
-                jigsawRewardDuration: 365 days,
+                jigsawRewardDuration: DEFAULT_REWARDS_DURATION,
                 tokenIn: 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48, //USDC
                 tokenOut: 0x09D4214C03D01F49544C0448DBE3A27f768F2b34
             })
@@ -333,9 +344,54 @@ contract CommonStrategyScriptBase is Script {
         // Populate the individual initialization params per each Pendle strategy, e.g.:
         pendleStrategyParams.push(
             PendleStrategyParams({
-                pendleMarket: 0x676106576004EF54B4bD39Ce8d2B34069F86eb8f,
-                jigsawRewardDuration: 365 days,
-                tokenIn: 0xD9A442856C234a39a81a089C06451EBAa4306a72,
+                pendleMarket: 0xB162B764044697cf03617C2EFbcB1f42e31E4766,
+                jigsawRewardDuration: DEFAULT_REWARDS_DURATION,
+                tokenIn: 0x9D39A5DE30e57443BfF2A8307A4256c8797A3497, // sUSDe
+                rewardToken: 0x808507121B80c02388fAd14726482e061B8da827
+            })
+        );
+
+        pendleStrategyParams.push(
+            PendleStrategyParams({
+                pendleMarket: 0x048680F64d6DFf1748ba6D9a01F578433787e24B,
+                jigsawRewardDuration: DEFAULT_REWARDS_DURATION,
+                tokenIn: 0x35D8949372D46B7a3D5A56006AE77B215fc69bC0, // USD0++
+                rewardToken: 0x808507121B80c02388fAd14726482e061B8da827
+            })
+        );
+
+        pendleStrategyParams.push(
+            PendleStrategyParams({
+                pendleMarket: 0x08a152834de126d2ef83D612ff36e4523FD0017F,
+                jigsawRewardDuration: DEFAULT_REWARDS_DURATION,
+                tokenIn: 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0, // wstETH
+                rewardToken: 0x808507121B80c02388fAd14726482e061B8da827
+            })
+        );
+
+        pendleStrategyParams.push(
+            PendleStrategyParams({
+                pendleMarket: 0xfd5Cf95E8b886aCE955057cA4DC69466e793FBBE,
+                jigsawRewardDuration: DEFAULT_REWARDS_DURATION,
+                tokenIn: 0xFAe103DC9cf190eD75350761e95403b7b8aFa6c0, // rswETH
+                rewardToken: 0x808507121B80c02388fAd14726482e061B8da827
+            })
+        );
+
+        pendleStrategyParams.push(
+            PendleStrategyParams({
+                pendleMarket: 0x58612beB0e8a126735b19BB222cbC7fC2C162D2a,
+                jigsawRewardDuration: DEFAULT_REWARDS_DURATION,
+                tokenIn: 0xD9A442856C234a39a81a089C06451EBAa4306a72, // pufETH
+                rewardToken: 0x808507121B80c02388fAd14726482e061B8da827
+            })
+        );
+
+        pendleStrategyParams.push(
+            PendleStrategyParams({
+                pendleMarket: 0x931F7eA0c31c14914a452d341bc5Cb5d996BE71d,
+                jigsawRewardDuration: DEFAULT_REWARDS_DURATION,
+                tokenIn: 0x8236a87084f8B84306f72007F36F2618A5634494, // LBTC
                 rewardToken: 0x808507121B80c02388fAd14726482e061B8da827
             })
         );
@@ -347,7 +403,7 @@ contract CommonStrategyScriptBase is Script {
             DineroStrategyParams({
                 pirexEth: 0xD664b74274DfEB538d9baC494F3a4760828B02b0,
                 autoPirexEth: 0x9Ba021B0a9b958B5E75cE9f6dff97C7eE52cb3E6,
-                jigsawRewardDuration: 365 days,
+                jigsawRewardDuration: DEFAULT_REWARDS_DURATION,
                 tokenIn: 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2, //wETH
                 tokenOut: 0x9Ba021B0a9b958B5E75cE9f6dff97C7eE52cb3E6
             })
