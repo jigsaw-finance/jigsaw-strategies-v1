cdc65a46e894836072d8bb73d92c09a29842c5f7
Hovooo <avakiandev@gmail.com>
2025-03-05 11:57:21 +0530

    Fixed HAL-02 finding

--- src/reservoir/ReservoirSavingStrategy.sol
+++ src/reservoir/ReservoirSavingStrategy.sol
@@ -3,7 +3,6 @@ pragma solidity 0.8.22;
 
 import { IERC20, IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import { Math } from "@openzeppelin/contracts/utils/math/Math.sol";
 
 import { IHolding } from "@jigsaw/src/interfaces/core/IHolding.sol";
 import { IManagerContainer } from "@jigsaw/src/interfaces/core/IManagerContainer.sol";
