d56960a1d178125fd8522c27c91cda6c75168a67
Hovooo <avakiandev@gmail.com>
2025-04-18 13:41:53 +0530

    Updated tests [WIP]

--- test/elixir/ElixirStrategy_fork.t.sol
+++ test/elixir/ElixirStrategy_fork.t.sol
@@ -4,21 +4,22 @@ pragma abicoder v2;
 
 import "../fixtures/BasicContractsFixture.t.sol";
 
-import {stdStorage, StdStorage} from "forge-std/Test.sol";
+import { StdStorage, stdStorage } from "forge-std/Test.sol";
 
-import {ElixirStrategy} from "../../src/elixir/ElixirStrategy.sol";
-import {IstdeUSD} from "../../src/elixir/interfaces/IstdeUSD.sol";
-import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
-import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import { ElixirStrategy } from "../../src/elixir/ElixirStrategy.sol";
 
-import {IERC20, IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+import { ERC20Mock } from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import { IERC20, IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
-import {StakerLight} from "../../src/staker/StakerLight.sol";
-import {StakerLightFactory} from "../../src/staker/StakerLightFactory.sol";
+import { StakerLight } from "../../src/staker/StakerLight.sol";
+import { StakerLightFactory } from "../../src/staker/StakerLightFactory.sol";
 
 contract ElixirStrategyTest is Test, BasicContractsFixture {
     using SafeERC20 for IERC20;
     // Mainnet USDT
+
     address internal tokenIn = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     // sdeUSD token
     address internal tokenOut = 0x5C5b196aBE0d54485975D1Ec29617D42D9198326;
@@ -39,7 +40,7 @@ contract ElixirStrategyTest is Test, BasicContractsFixture {
         address strategyImplementation = address(new ElixirStrategy());
         ElixirStrategy.InitializerParams memory initParams = ElixirStrategy.InitializerParams({
             owner: OWNER,
-            managerContainer: address(managerContainer),
+            manager: address(manager),
             stakerFactory: address(stakerFactory),
             jigsawRewardToken: jRewards,
             jigsawRewardDuration: 60 days,
@@ -59,15 +60,27 @@ contract ElixirStrategyTest is Test, BasicContractsFixture {
         strategyManager.addStrategy(address(strategy));
 
         SharesRegistry tokenInSharesRegistry = new SharesRegistry(
-            OWNER, address(managerContainer), address(tokenIn), address(usdcOracle), bytes(""), 50_000
+            OWNER,
+            address(manager),
+            address(tokenIn),
+            address(usdcOracle),
+            bytes(""),
+            ISharesRegistry.RegistryConfig({
+                collateralizationRate: 50_000,
+                liquidationBuffer: 5e3,
+                liquidatorBonus: 8e3
+            })
         );
+
         stablesManager.registerOrUpdateShareRegistry(address(tokenInSharesRegistry), address(tokenIn), true);
         registries[address(tokenIn)] = address(tokenInSharesRegistry);
         vm.stopPrank();
     }
 
     // Tests if deposit works correctly when authorized
-    function test_elixir_deposit_when_authorized(uint256 _amount) public notOwnerNotZero(user) {
+    function test_elixir_deposit_when_authorized(
+        uint256 _amount
+    ) public notOwnerNotZero(user) {
         // added to prevent USDT safeTransferFrom revert issue
         uint256 amount = bound(_amount, 1e6, 10e6);
         address userHolding = initiateUser(user, tokenIn, amount);
@@ -77,12 +90,8 @@ contract ElixirStrategyTest is Test, BasicContractsFixture {
 
         // Invest into the tested strategy vie strategyManager
         vm.prank(user, user);
-        (uint256 receiptTokens, uint256 tokenInAmount) = strategyManager.invest(
-            tokenIn,
-            address(strategy),
-            amount,
-            abi.encodePacked(tokenIn, poolFee, deUSD)
-        );
+        (uint256 receiptTokens, uint256 tokenInAmount) =
+            strategyManager.invest(tokenIn, address(strategy), amount, 0, abi.encodePacked(tokenIn, poolFee, deUSD));
 
         uint256 tokenOutBalanceAfter = IERC20(tokenOut).balanceOf(userHolding);
         uint256 expectedShares = tokenOutBalanceAfter - tokenOutBalanceBefore;
@@ -99,10 +108,8 @@ contract ElixirStrategyTest is Test, BasicContractsFixture {
         assertEq(IERC20(tokenIn).balanceOf(userHolding), tokenInBalanceBefore - amount, "Holding tokenIn balance wrong");
         // allow 5% difference for tokenOut balance
         assertApproxEqRel(
-            IERC20(tokenOut).balanceOf(userHolding),
-            amount * 1e12,
-            0.05e18,
-            "Holding token out balance wrong");
+            IERC20(tokenOut).balanceOf(userHolding), amount * 1e12, 0.05e18, "Holding token out balance wrong"
+        );
         assertEq(
             IERC20(address(strategy.receiptToken())).balanceOf(userHolding),
             expectedShares,
@@ -123,35 +130,33 @@ contract ElixirStrategyTest is Test, BasicContractsFixture {
     }
 
     // Tests if withdraw works correctly when authorized
-    function test_elixir_withdraw_when_authorized(uint256 _amount) public notOwnerNotZero(user) {
+    function test_elixir_withdraw_when_authorized(
+        uint256 _amount
+    ) public notOwnerNotZero(user) {
         // added to prevent USDT safeTransferFrom revert issue
-        uint256 amount = bound(_amount, 1e6, 100000e6);
+        uint256 amount = bound(_amount, 1e6, 100_000e6);
         address userHolding = initiateUser(user, tokenIn, amount);
 
         // Invest into the tested strategy vie strategyManager
         vm.prank(user, user);
-        (uint256 receiptTokens, uint256 tokenInAmount) = strategyManager.invest(
-            tokenIn,
-            address(strategy),
-            amount,
-            abi.encodePacked(tokenIn, poolFee, deUSD)
-        );
 
-        (uint256 investedAmountBefore, uint256 totalShares) = strategy.recipients(userHolding);
+        strategyManager.invest(tokenIn, address(strategy), amount, 0, abi.encodePacked(tokenIn, poolFee, deUSD));
+
+        (, uint256 totalShares) = strategy.recipients(userHolding);
         uint256 tokenInBalanceBefore = IERC20(tokenIn).balanceOf(userHolding);
 
-        _transferInRewards(100000e18);
+        _transferInRewards(100_000e18);
         skip(90 days);
 
         strategy.cooldown(userHolding, totalShares);
         skip(7 days);
 
         vm.prank(user, user);
-        (uint256 assetAmount,) = strategyManager.claimInvestment({
+        strategyManager.claimInvestment({
             _holding: userHolding,
+            _token: tokenIn,
             _strategy: address(strategy),
             _shares: totalShares,
-            _asset: tokenIn,
             _data: abi.encodePacked(deUSD, poolFee, tokenIn)
         });
 
@@ -168,7 +173,7 @@ contract ElixirStrategyTest is Test, BasicContractsFixture {
          * 5. Strategy's total shares  -= shares
          * 6. Fee address fee amount += yield * performanceFee
          */
-        assertEq(tokenInBalanceAfter, assetAmount, "Holding balance after withdraw is wrong");
+        assertEq(tokenInBalanceAfter, expectedWithdrawal, "Holding balance after withdraw is wrong");
         assertEq(IERC20(tokenOut).balanceOf(userHolding), 0, "Holding token out balance wrong");
         assertEq(
             IERC20(address(strategy.receiptToken())).balanceOf(userHolding),
@@ -182,20 +187,98 @@ contract ElixirStrategyTest is Test, BasicContractsFixture {
         assertEq(tokenInBalanceAfter, expectedWithdrawal, "Incorrect asset amount returned");
     }
 
-    function _transferInRewards(uint256 _amount) internal {
-        address defaultAdmin = strategy.stdeUSD().owner();
+    function _transferInRewards(
+        uint256 _amount
+    ) internal {
+        IStdeUSD stdeUSD = IStdeUSD(address(strategy.stdeUSD()));
+        address defaultAdmin = stdeUSD.owner();
 
         address rewarder = vm.randomAddress();
         deal(deUSD, rewarder, _amount);
 
         vm.startPrank(defaultAdmin);
-        strategy.stdeUSD().grantRole(keccak256("REWARDER_ROLE"), rewarder);
+        stdeUSD.grantRole(keccak256("REWARDER_ROLE"), rewarder);
         vm.stopPrank();
 
         vm.startPrank(rewarder, rewarder);
         IERC20(deUSD).approve(tokenOut, _amount);
 
-        strategy.stdeUSD().transferInRewards(_amount);
+        stdeUSD.transferInRewards(_amount);
         vm.stopPrank();
     }
+}
+
+interface IStdeUSD is IERC4626 {
+    /*//////////////////////////////////////////////////////////////
+                                 EVENTS
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice Event emitted when the rewards are received
+    event RewardsReceived(uint256 amount);
+    /// @notice Event emitted when the balance from an FULL_RESTRICTED_STAKER_ROLE user are redistributed
+    event LockedAmountRedistributed(address indexed from, address indexed to, uint256 amount);
+
+    /// @notice Event emitted when cooldown duration updates
+    event CooldownDurationUpdated(uint24 previousDuration, uint24 newDuration);
+
+    /*//////////////////////////////////////////////////////////////
+                                 ERRORS
+    //////////////////////////////////////////////////////////////*/
+    /// @notice Error emitted shares or assets equal zero.
+    error InvalidAmount();
+    /// @notice Error emitted when owner attempts to rescue deUSD tokens.
+    error InvalidToken();
+    /// @notice Error emitted when a small non-zero share amount remains, which risks donations attack
+    error MinSharesViolation();
+    /// @notice Error emitted when owner is not allowed to perform an operation
+    error OperationNotAllowed();
+    /// @notice Error emitted when there is still unvested amount
+    error StillVesting();
+    /// @notice Error emitted when owner or blacklist manager attempts to blacklist owner
+    error CantBlacklistOwner();
+    /// @notice Error emitted when the zero address is given
+    error InvalidZeroAddress();
+
+    /// @notice Error emitted when the shares amount to redeem is greater than the shares balance of the owner
+    error ExcessiveRedeemAmount();
+    /// @notice Error emitted when the shares amount to withdraw is greater than the shares balance of the owner
+    error ExcessiveWithdrawAmount();
+    /// @notice Error emitted when cooldown value is invalid
+    error InvalidCooldown();
+    error AccessControlUnauthorizedAccount(address account, bytes32 neededRole);
+
+    /*//////////////////////////////////////////////////////////////
+                            EXTERNAL FUNCTIONS
+    //////////////////////////////////////////////////////////////*/
+    function cooldownDuration() external returns (uint24);
+
+    function owner() external returns (address);
+
+    function transferInRewards(
+        uint256 amount
+    ) external;
+
+    function rescueTokens(address token, uint256 amount, address to) external;
+
+    function getUnvestedAmount() external view returns (uint256);
+
+    function cooldownAssets(
+        uint256 assets
+    ) external returns (uint256 shares);
+
+    function cooldownShares(
+        uint256 shares
+    ) external returns (uint256 assets);
+
+    function unstake(
+        address receiver
+    ) external;
+
+    function setCooldownDuration(
+        uint24 duration
+    ) external;
+
+    function withdraw(uint256 assets, address receiver, address _owner) external returns (uint256);
+
+    function grantRole(bytes32 role, address account) external;
 }
