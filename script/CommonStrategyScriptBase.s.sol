afcf238d6a19c3cfde4da5747743a5c171db3bcd
Hovooo <avakiandev@gmail.com>
2025-08-23 13:06:28 +0200

    Deployed Aave USDC strategy

--- script/CommonStrategyScriptBase.s.sol
+++ script/CommonStrategyScriptBase.s.sol
@@ -326,17 +326,8 @@ contract CommonStrategyScriptBase is Script, ValidateInterface {
             AaveStrategyParams({
                 rewardToken: address(0),
                 jigsawRewardDuration: DEFAULT_REWARDS_DURATION,
-                tokenIn: 0x039e2fB66102314Ce7b64Ce5Ce3E5183bc94aD38, //wS
-                tokenOut: 0x6C5E14A212c1C3e4Baf6f871ac9B1a969918c131
-            })
-        );
-
-        aaveStrategyParams.push(
-            AaveStrategyParams({
-                rewardToken: address(0),
-                jigsawRewardDuration: DEFAULT_REWARDS_DURATION,
-                tokenIn: 0x50c42dEAcD8Fc9773493ED674b675bE577f2634b, //WETH
-                tokenOut: 0xe18Ab82c81E7Eecff32B8A82B1b7d2d23F1EcE96
+                tokenIn: 0x29219dd400f2Bf60E5a23d13Be72B486D4038894, //USDC
+                tokenOut: 0x578Ee1ca3a8E1b54554Da1Bf7C583506C4CD11c6
             })
         );
     }
